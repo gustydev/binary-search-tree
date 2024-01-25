2d82b58c951c8b5e74a10ed1f6c499691c346a15
gusty <gustavodiorlando@gmail.com>
2024-01-25 12:34:33 -0300

    Create initial build tree function

--- bst.js
+++ bst.js
@@ -1,10 +1,38 @@
 class Node {
-    constructor(data = null, left = null, right = null) {
+    constructor(data, left, right) {
         this.data = data;
-        this.left = [left];
-        this.right = [right];
+        this.left = left;
+        this.right = right;
     }
 }
 
-const testnode = new Node('panda');
-console.log(testnode)
+function sortArray(arr) {
+    arr.sort((a, b) => { return a - b }); // Sorts numerically
+    arr = [...new Set(arr)]; // Removes duplicates
+    return arr;
+}
+
+class Tree {
+    constructor(arr) {
+        this.root = this.buildTree(sortArray(arr));
+    }
+    buildTree(arr, start = 0, end = arr.length - 1) {
+        if (start > end) {
+            return null; // Base case
+        }
+        
+        const mid = (start + end) / 2;
+        const root = new Node(arr[mid]);
+
+        root.left = this.buildTree(arr, start, mid - 1);
+        root.right = this.buildTree(arr, mid + 1, end);
+
+        return root;
+    }
+}
+
+// [0, 1, 2, 3, 4]
+// root = 2, left [0, 1], right [3, 4]
+const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]; // After sorting and getting rid of dupes, length 11. End = index 10
+const tree = new Tree(testArr);
+console.log(tree)
